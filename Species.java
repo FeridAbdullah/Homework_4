45fb7ec24b461b06aad4e47c8500f6cfa5e5a19e
FeridAbdullah <farid.z.guliyev.std@bhos.edu.az>
2025-02-22 22:06:09 +0400

    Species.java

---
+++ Species.java
@@ -0,0 +1,15 @@
+public enum Species {
+    DOG(true, 4, true),
+    CAT(false, 4, true),
+    PARROT(true, 2, false);
+
+    private final boolean canFly;
+    private final int numberOfLegs;
+    private final boolean hasFur;
+
+    Species(boolean canFly, int numberOfLegs, boolean hasFur) {
+        this.canFly = canFly;
+        this.numberOfLegs = numberOfLegs;
+        this.hasFur = hasFur;
+    }
+}
